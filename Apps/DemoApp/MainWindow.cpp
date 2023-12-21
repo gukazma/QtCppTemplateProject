db208fb5a9829144a45caeb5452f249e4b0bad4b
gukazma <gukazma@proton.me>
2023-12-21 16:00:28 +0800

    🔧 build: close windows ui

--- Apps/DemoApp/MainWindow.cpp
+++ Apps/DemoApp/MainWindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::clicked, [&]() { 
+        ui->label->setText("2");
+    });
+    connect(ui->pushButton_2, &QPushButton::clicked, [&]() { ui->label->setText("4"); });
 }
 
 MainWindow::~MainWindow()
