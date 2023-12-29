6e0b6f2c6d2cf487772e0268e4951609ffb1861b
gukazma <gukazma@proton.me>
2023-12-29 16:46:24 +0800

    ✨ feat: translate icon

--- Apps/DemoApp/main.cpp
+++ Apps/DemoApp/main.cpp
@@ -1,13 +1,21 @@
-#include <QApplication>
 #include "MainWindow.h"
-
+#include <QApplication>
+#include <QSplashScreen>
+#include <QTranslator>
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
+    QApplication  app(argc, argv);
+    QPixmap       pixmap(QString::fromLocal8Bit(":/mgukazma.jpg"));
+    QSplashScreen splash(pixmap);
+    splash.show();
 
-    MainWindow mainwinndow;
+    QTranslator* translator = new QTranslator;
+    translator->load(":/Translations/zh_CN.qm");
+    app.installTranslator(translator);
 
-    mainwinndow.show();
+    MainWindow mainwindow;
+    mainwindow.showMaximized();
 
+    splash.finish(&mainwindow);
     return app.exec();
 }
